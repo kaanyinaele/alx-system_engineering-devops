5721e19c42dcb2da14957d70613de707265e1475
kaanyinaele <kaanyinaele@gmail.com>
2024-06-21 15:29:10 +0100

    Sky is the limit, let's bring that limit higher

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,19 +1,14 @@
 # Increases the amount of traffic an Nginx server can handle
 
-# Define the exec resource to modify ULIMIT in /etc/default/nginx
+# Increase the ULIMIT of the default file
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/^ulimit -n 15/ulimit -n 4096/" /etc/default/nginx',
-  path	=> '/usr/bin:/bin',  # Ensure correct path to 'sed'
-  onlyif  => 'grep "^ulimit -n 15" /etc/default/nginx',  # Only execute if the line exists
-  notify  => Exec['nginx-restart'],  # Notify the nginx-restart exec to restart Nginx after this change
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
 }
 
-# Define the exec resource to restart Nginx
+#restart Nginx
+
 exec { 'nginx-restart':
-  command 	=> '/etc/init.d/nginx restart',  # Command to restart Nginx
-  path    	=> '/sbin:/usr/sbin:/bin:/usr/bin',  # Ensure correct path for Nginx restart command
-  refreshonly => true,  # Only run this exec if notified
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
-
-
-
