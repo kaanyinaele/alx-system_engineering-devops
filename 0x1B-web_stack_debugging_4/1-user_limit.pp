7f422cbb4727fc6cec1a2691b80b2f2f4785d83b
kaanyinaele <kaanyinaele@gmail.com>
2024-06-21 15:37:02 +0100

    User limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,21 @@
-#Enable the user holberton to login and open files without errors
-
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+# Increase file descriptor limit for the holberton user
+exec { 'increase-file-descriptor-limit-hard':
+  command => 'echo "holberton hard nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
+  unless  => 'grep "holberton hard nofile 50000" /etc/security/limits.conf',
 }
 
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+exec { 'increase-file-descriptor-limit-soft':
+  command => 'echo "holberton soft nofile 50000" >> /etc/security/limits.conf',
+  path    => ['/usr/bin', '/bin'],
+  unless  => 'grep "holberton soft nofile 50000" /etc/security/limits.conf',
 }
+
+# Restart session for the changes to take effect
+exec { 'apply-limits':
+  command     => 'su - holberton -c "ulimit -n 50000"',
+  path        => ['/usr/bin', '/bin'],
+  subscribe   => [Exec['increase-file-descriptor-limit-hard'], Exec['increase-file-descriptor-limit-soft']],
+  refreshonly => true,
+}
+
